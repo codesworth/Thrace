143227ce19d94eb2a0ea484f7df0f5fb7f95aa9f
codesworth <codezworth@gmail.com>
2019-01-07 22:48:54 +0000

    Added Lexers and Base ASTs

---
+++ AST/ExprAST.h
@@ -0,0 +1,97 @@
+//
+// Created by Mensah Shadrach on 07/01/2019.
+//
+
+#include <string>
+#include <vector>
+
+
+class ExprAST{
+
+public:
+    virtual ~ExprAST(){}
+
+};
+
+
+class NumberExprAST:public ExprAST{
+    double  Val;
+
+public:
+    NumberExprAST(double val):Val(val){}
+};
+
+
+/// VariableExprAST - Expression class for referencing a variable, like "a".
+
+class VariableExprAST: public ExprAST{
+    std::string Name;
+
+public:
+    VariableExprAST(const std::string &Name): Name(Name){}
+};
+
+/// BinaryExprAST - Expression class for a binary operator.
+
+class BinaryExprAST:public ExprAST{
+
+    char Op;
+    std::unique_ptr<ExprAST> LHS,RHS;
+
+public:
+    BinaryExprAST(char op, std::unique_ptr<ExprAST>LHS, std::unique_ptr<ExprAST> RHS):
+            Op(op),LHS (std::move(LHS)), RHS(std::move(RHS)){}
+
+
+};
+
+
+/// CallExprAST - Expression class for function calls.
+
+class CallExprAST:public ExprAST{
+
+    std::string Callee;
+    std::vector<std::unique_ptr<ExprAST>> Args;
+
+    CallExprAST(std::string &callee, std::vector<std::unique_ptr<ExprAST>> Args):
+            Callee(callee),Args(std::move(Args)){}
+
+
+};
+
+
+/// PrototypeAST - This class represents the "prototype" for a function,
+/// which captures its name, and its argument names (thus implicitly the number
+/// of arguments the function takes).
+
+
+class PrototypeAST{
+
+    std::string Name;
+    std::vector<std::string>Args;
+
+
+public:
+    PrototypeAST(std::string name, std::vector<std::string> args):
+            Name(name), Args(args){}
+
+    std::string &getName () const{
+        return Name;
+    }
+
+};
+
+
+
+/// FunctionAST - This class represents a function definition itself.
+
+class FunctionAST{
+
+    std::unique_ptr<PrototypeAST> Proto;
+    std::unique_ptr<ExprAST> Body;
+
+public:
+    FunctionAST(std::unique_ptr<PrototypeAST> proto, std::unique_ptr<ExprAST> body):
+      Proto(std::move(proto)), Body(std::move(body)){}
+};
+
--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include <string>
+#include "AST/ExprAST.h"
+#include
 
+
+/// CurTok/getNextToken - Provide a simple token buffer.  CurTok is the current
+/// token the parser is looking at.  getNextToken reads another token from the
+/// lexer and updates CurTok with its results.
 
 enum Token{
     tok_eof = -1,
@@ -17,6 +23,84 @@ enum Token{
 static  std::string IdentifierStr;
 static  double  NumVal;
 
+
+static int getToken(){
+    static int lastChar = ' ';
+
+    while (isspace(lastChar)){
+        lastChar = getchar();
+    }
+
+    if (isalpha(lastChar)){
+        IdentifierStr = lastChar;
+        while (isalnum(lastChar = getchar()))
+            IdentifierStr = lastChar;
+
+
+        if (IdentifierStr == "def"){
+            return toke_def;
+        }
+        if (IdentifierStr == "extern")
+            return  tok_extern;
+
+        return  tok_identifier;
+
+    }
+
+    if (isdigit(lastChar) || lastChar == '.'){
+        std::string Numstr;
+
+        do{
+            Numstr += lastChar;
+            lastChar = getchar();
+
+        }while (isdigit(lastChar) || lastChar == '.');
+
+        NumVal = strtod(Numstr.c_str(),0);
+        return  tok_number;
+    }
+    //Handle Comments
+    if (lastChar == '#'){
+        do
+            lastChar = getchar();
+        while (lastChar != EOF && lastChar != '\n' && lastChar != '\r');
+
+        if (lastChar != EOF){
+            return getToken();
+        }
+    }
+
+    if (lastChar == EOF)
+        return  tok_eof;
+    // Otherwise, just return the character as its ascii value.
+    int Thischar = lastChar;
+    lastChar = getchar();
+    return Thischar;
+}
+
+static  int CurTok;
+static int getNextTok(){
+
+    return CurTok = getToken();
+}
+
+
+/// LogError* - These are little helper functions for error handling.
+
+std::unique_ptr<ExprAST> LogErr(const char *str){
+    fprintf(stderr,"LogError: %s\n");
+    return nullptr;
+}
+
+
+std::unique_ptr<PrototypeAST>LogErrorP(const char *str){
+    LogErr(str);
+    return nullptr;
+}
+
+static std::unique_ptr<ExprAST> ParseNumberExpr(){
+    auto Result llvm
+}
 
 
 int main() {
