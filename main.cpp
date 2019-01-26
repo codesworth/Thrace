7bf57f93b32c20127d62d946205adba817203248
codesworth <codezworth@gmail.com>
2019-01-26 23:47:57 +0000

    Running First Build

--- main.cpp
+++ main.cpp
@@ -1,26 +1,11 @@
 #include <iostream>
-
-#include "AST/ExprAST.h"
-#include <llvm/Analysis/AliasAnalysis.h>
-
-
-
-
-/// CurTok/getNextToken - Provide a simple token buffer.  CurTok is the current
-/// token the parser is looking at.  getNextToken reads another token from the
-/// lexer and updates CurTok with its results.
-
-
-
-
-
-
-
-
+#include "Parsing/BasicParser.h"
 
 
 int main() {
+    BasicParser Parser = BasicParser();
 
+    Parser.MainLoop();
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
